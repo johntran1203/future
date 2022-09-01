1f41740fb8b1b2bbe25d4485000d49ca02ec9c82
johntran <tranjohn1211@gmail.com>
2022-09-01 11:32:32 -0700

    create food and connect server

--- controllers/food.js
+++ controllers/food.js
@@ -9,6 +9,32 @@ const getAllFoods = async (req, res) => {
     }
 }
 
+// GET/food/:id
+const getFoodById = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const food = await Food.findById(id);
+        res.json(food);
+        } catch (e) {
+            res.status(500).json({ error: e.message });
+        }
+}
+
+//POST /food
+const createFood = async (req, res) => {
+    try {
+        // make a new food by using the constructor
+        const food = new Food(req.body);
+        await food.save()
+        res.status(201).json(food);
+
+    } catch (e) {
+        res.status(500).json({ error: e.message});
+    }
+}
+
 module.exports = {
     getAllFoods,
+    getFoodById,
+    createFood,
 }
--- routes/food.js
+++ routes/food.js
@@ -7,5 +7,10 @@ const foodRouter = new Router();
 // make our GET route "/" and ref our controller's getALLfood function
 foodRouter.get('/', foodController.getAllFoods);
 
+// make a Get /:id route and ref our controller's getFoodById function
+foodRouter.get("/:id", foodController.getFoodById);
+
+// make a POSt/ route and ref our controller's food function
+foodRouter.post('/',foodController.createFood);
 // export to use elsewhere
 module.exports = foodRouter;
