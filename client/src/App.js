b82eead77dd88c6d7209f5854714f9d32645e380
johntran <tranjohn1211@gmail.com>
2022-09-01 15:34:02 -0700

    create routes create navbar initialize client

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { Nav } from './components/Nav';
+import {Home} from './components/Home'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+      <Route path="/" element={<Home />} />
+      </Routes>
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/data.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { FaLinkedin, FaTwitter, FaEnvelope, FaRegNewspaper } from 'react-icons/fa'
+export const links = [
+  {
+    id: 1,
+    url: '/',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    url: '/about',
+    text: 'About',
+  },
+  {
+    id: 3,
+    url: '/contact',
+    text: 'Contact',
+  },
+  {
+    id: 4,
+    url: '/table',
+    text: 'Table',
+  },
+  {
+    id: 5,
+    url: '/pictures',
+    text: 'pictures',
+  },
+  {
+    id: 6,
+    url: '/info',
+    text: 'Info',
+  },
+]
+
+export const social = [
+  {
+    id: 1,
+    url: 'https://twitter.com/Johntran1203',
+    icon: <FaTwitter />,
+  },
+  {
+    id: 2,
+    url: 'https://johntran-portfolio.netlify.app/',
+    icon: <FaRegNewspaper />,
+  },
+  {
+    id: 3,
+    url: 'https://www.linkedin.com/in/john-tran123/',
+    icon: <FaLinkedin/>
+  },
+  {
+    id: 4,
+    url: 'https://www.twitter.com',
+    icon: <FaEnvelope />,
+  },
+]
+
+export const definition = [
+    {
+      id: 1,
+      title: 'What is table tennis (ping pong)?',
+      answer:
+        'Table tennis, also known as ping-pong and whiff-whaff, is a sport in which two or four players hit a lightweight ball, also known as the ping-pong ball, back and forth across a table using small solid rackets. The game takes place on a hard table divided by a net. Except for the initial serve, the rules are generally as follows: players must allow a ball played toward them to bounce once on their side of the table and must return it so that it bounces on the opposite side at least once. A point is scored when a player fails to return the ball within the rules.',
+    },
+    {
+      id: 2,
+      title: 'What is pool?',
+      answer:
+        'Pool is a classification of cue sports played on a table with six pockets along the rails, into which balls are deposited. Each specific pool game has its own name; some of the better-known include eight-ball, blackball, nine-ball, ten-ball, seven-ball, straight pool, one-pocket, and bank pool.',
+    },
+    {
+      id: 3,
+      title: 'What is DDR (Dance Dance Revolution)?',
+      answer:
+        'Dance Dance Revolution is the pioneering series of the rhythm and dance genre in video games. Players stand on a "dance platform" or stage and hit colored arrows laid out in a cross with their feet to musical and visual cues. Players are judged by how well they time their dance to the patterns presented to them and are allowed to choose more music to play to if they receive a passing score.',
+    },
+  ]
