cd42baadb8fa6b394a70951f5434c4cef9ea6960
johntran <tranjohn1211@gmail.com>
2022-09-06 19:59:01 -0700

    add social media icon

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,9 +1,27 @@
 import { Link } from "react-router-dom";
+import React, { useState, useRef, useEffect } from "react";
+import { links, social } from "./data";
+import { FaBars } from "react-icons/fa";
+
 export const Nav = () => {
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
   return (
-    <div>
-      <Link to="/"> Home </Link>
-      <Link to="/newFood"> Enter food </Link>
-    </div>
+    <nav>
+      <div>
+        <Link to="/"> Home </Link>
+        <Link to="/newFood"> Enter food </Link>
+      </div>
+      <ul className="social-icons">
+        {social.map((socialIcon) => {
+          const {id, url, icon} = socialIcon
+          return (
+            <li key={id}><a href={url}>{icon}</a></li>
+          )
+        })}
+      </ul>
+    </nav>
   );
 };
