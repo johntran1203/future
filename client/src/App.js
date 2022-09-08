20ee6570c37662a85f89a6d82314cc6ef70157d8
johntran <tranjohn1211@gmail.com>
2022-09-08 01:16:14 -0700

    add a contact and route

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,22 @@
-
-import './App.css';
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { Nav } from './components/Nav';
-import { Home} from './screens/Home';
-import { FoodDetail} from './screens/FoodDetail';
-import { NewLocation } from './screens/NewLocation';
-
+import { Nav } from "./components/Nav";
+import { Home } from "./screens/Home";
+import { Contact } from "./components/Contact";
+import { FoodDetail } from "./screens/FoodDetail";
+import { NewLocation } from "./screens/NewLocation";
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/foods/:id"  element={<FoodDetail/> }/>
-      <Route path="/newFood" element={<NewLocation />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/foods/:id" element={<FoodDetail />} />
+        <Route path="/newFood" element={<NewLocation />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
       </Routes>
-  
     </div>
   );
 }
