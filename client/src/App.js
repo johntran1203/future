08c1134f67ec2f50e9a6e68b281cac9792c07c48
johntran <tranjohn1211@gmail.com>
2022-09-15 13:34:35 -0700

    add used context to make one variable global

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, createContext } from "react";
 import { Routes, Route } from "react-router-dom";
 import { Nav } from "./components/Nav";
 import { Home } from "./screens/Home";
@@ -8,11 +8,15 @@ import { FoodDetail } from "./screens/FoodDetail";
 import { NewLocation } from "./screens/NewLocation";
 import Register from "./screens/Register";
 
+export const AppContext = createContext();
+
 function App() {
   const [user, setUser] = useState(null);
+  const [userName, setUserName] = useState("john")
 
   return (
     <div className="App">
+      <AppContext.Provider value={{userName, setUserName}}>
       <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -23,6 +27,7 @@ function App() {
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
       </Routes>
+      </AppContext.Provider>
     </div>
   );
 }
--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -1,10 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useContext } from 'react';
 import { Link } from "react-router-dom";
+import { AppContext } from '../App';
 import { getFoods } from '../services';
 
 export const Home = () => {
+    const{ userName } = useContext(AppContext)
     const [allFoods, setAllFoods] = useState([]);
-    console.log(allFoods)
     // 1. on mount this component should fetch all cakes and save it to some state
     useEffect(() => {
         // call getFoods and save into state (async)
@@ -17,11 +18,13 @@ export const Home = () => {
     }, []);
     // 2. map through all the foods in state and render a Link for each one, leading to the food detail page
     return (
+
     <section>
         {allFoods.map((food) => (
             <Link to={`/foods/${food._id}`}>{food.location}</Link>
             // <h2 key={food}>{food.location}</h2>
         ))}
+        <h3>Hello my name is {userName}</h3>
     </section>
     // 3. profit!
     )
